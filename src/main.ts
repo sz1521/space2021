06d951758c92238e82a3e170200c900d0d4d034b
Tero Jäntti <tero.jantti@tieto.com>
2021-08-26 17:06:19 +0300

    Simple tile map.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,42 @@
-import { init, Sprite, GameLoop } from "kontra";
+import { init, Sprite, GameLoop, TileEngine } from "kontra";
 
 const { canvas } = init();
+
+let tileEngine: TileEngine | null = null;
+
+const img = new Image();
+img.src = 'tiles.png';
+img.onload = function() {
+  tileEngine = TileEngine({
+    tilewidth: 32,
+    tileheight: 32,
+
+    width: 14,
+    height: 12,
+
+    tilesets: [{
+      firstgid: 1,
+      image: img
+    }],
+
+    layers: [{
+      name: 'ground',
+      data: [ 2,  2,  2,  2,  1,  2,  2,  3,  2,  3,  1,  3,  2,  3,
+              1,  2,  2,  2,  2,  3,  2,  2,  2,  2,  1,  3,  1,  2,
+              2,  1,  1,  3,  3,  2,  2,  2,  2,  1,  3,  2,  3,  3,
+              1,  3,  1,  1,  2,  3,  3,  2,  3,  2,  1,  1,  1,  1,
+              2,  2,  3,  1,  2,  2,  2,  1,  3,  3,  2,  3,  3,  2,
+              2,  2,  3,  2,  3,  3,  3,  3,  3,  3,  3,  2,  1,  3,
+              2,  2,  1,  2,  2,  2,  1,  3,  1,  2,  3,  2,  3,  1,
+              3,  2,  2,  2,  2,  2,  1,  2,  2,  1,  2,  2,  1,  2,
+              3,  2,  2,  2,  2,  2,  3,  3,  1,  1,  3,  2,  3,  3,
+              2,  2,  3,  1,  1,  2,  2,  2,  1,  3,  2,  3,  3,  2,
+              2,  2,  2,  2,  3,  2,  2,  2,  2,  1,  2,  3,  1,  1,
+              2,  3,  3,  2,  2,  2,  1,  1,  2,  1,  1,  3,  2,  2,
+            ]
+    }]
+  });
+}
 
 const resize = () => {
   canvas.width = window.innerWidth - 10;
@@ -28,6 +64,9 @@ const loop = GameLoop({
     }
   },
   render: function () {
+    if (tileEngine) {
+      tileEngine.render();
+    }
     sprite.render();
   },
 });
