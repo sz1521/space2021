079ace288f6b3a10bc17ffffabc0bb0a5fbf807c
Tero Jäntti <tero.jantti@tieto.com>
2021-08-26 20:23:34 +0300

    Level class.

--- src/main.ts
+++ src/main.ts
@@ -22,41 +22,10 @@
  * SOFTWARE.
  */
 
-import { init, load, imageAssets, Sprite, GameLoop, TileEngine } from "kontra";
+import { Level } from "./Level";
+import { init, load, Sprite, GameLoop } from "kontra";
 
 const { canvas } = init();
-
-const createTileMap = (): TileEngine => {
-  return TileEngine({
-    tilewidth: 32,
-    tileheight: 32,
-
-    width: 14,
-    height: 12,
-
-    tilesets: [{
-      firstgid: 1,
-      image: imageAssets['tiles.png'],
-    }],
-
-    layers: [{
-      name: 'ground',
-      data: [ 2,  2,  2,  2,  1,  2,  2,  3,  2,  3,  1,  3,  2,  3,
-              1,  2,  2,  2,  2,  3,  2,  2,  2,  2,  1,  3,  1,  2,
-              2,  1,  1,  3,  3,  2,  2,  2,  2,  1,  3,  2,  3,  3,
-              1,  3,  1,  1,  2,  3,  3,  2,  3,  2,  1,  1,  1,  1,
-              2,  2,  3,  1,  2,  2,  2,  1,  3,  3,  2,  3,  3,  2,
-              2,  2,  3,  2,  3,  3,  3,  3,  3,  3,  3,  2,  1,  3,
-              2,  2,  1,  2,  2,  2,  1,  3,  1,  2,  3,  2,  3,  1,
-              3,  2,  2,  2,  2,  2,  1,  2,  2,  1,  2,  2,  1,  2,
-              3,  2,  2,  2,  2,  2,  3,  3,  1,  1,  3,  2,  3,  3,
-              2,  2,  3,  1,  1,  2,  2,  2,  1,  3,  2,  3,  3,  2,
-              2,  2,  2,  2,  3,  2,  2,  2,  2,  1,  2,  3,  1,  1,
-              2,  3,  3,  2,  2,  2,  1,  1,  2,  1,  1,  3,  2,  2,
-            ]
-    }]
-  });
-}
 
 const resize = () => {
   canvas.width = window.innerWidth - 10;
@@ -76,7 +45,7 @@ const sprite = Sprite({
 });
 
 load('tiles.png').then(() => {
-  const tileEngine = createTileMap();
+  const level = new Level();
 
   const loop = GameLoop({
     update: function () {
@@ -86,13 +55,14 @@ load('tiles.png').then(() => {
         sprite.x = -sprite.width;
       }
     },
+
     render: function () {
-      tileEngine.render();
+      level.render();
       sprite.render();
     },
   });
 
   loop.start();
 }).catch((error) => {
-  console.warn('Error loading tileset:', error);
+  console.warn('Error loading assets:', error);
 });
