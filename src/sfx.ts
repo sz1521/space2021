2ba00baf23eb928c2455e43d52c500e716bd02d0
Tero Jäntti <tero.jantti@tieto.com>
2021-09-12 15:40:57 +0300

    Playing sound effects.

--- src/sfx.ts
+++ src/sfx.ts
@@ -1,10 +1,26 @@
 import { TUNE } from './tune';
 
+// https://github.com/KilledByAPixel/ZzFX
+// https://killedbyapixel.github.io/ZzFX/
+
+// Generate a sound
+declare var zzfxG: (...data: any) => any[];
+
 // Play tune (from ZzFX)
 declare var zzfxP: (...parameters: any[]) => AudioBufferSourceNode;
 
 // Generate audio data for a song (from ZzFXM)
+// https://keithclark.github.io/ZzFXM/
 declare var zzfxM: Function;
+
+export const DESTROYCONE = zzfxG([,,33,.1,.02,.2,4,2.02,-36,5.9,,,.12,,,,,,.09]);
+export const DESTROYPLANT = zzfxG([2.47,,1626,,.01,0,1,2.21,-19,,,,.04,,,.3,.11,.68,.04,.09]);
+export const PLANTFLOWER = zzfxG([1.05,,1629,.03,.23,.01,1,2.79,23,6,,,.23,,-2.6,,,.25,.01]);
+export const PLANTVINE = zzfxG([2.01,,179,.14,.25,.01,1,1.48,,,,,,.3,,,.16,.11,.21,.1]);
+
+export const playEffect = (effect: any[]): void => {
+  zzfxP(effect);
+}
 
 export const renderSong = (): any => {
   return zzfxM(...TUNE);
